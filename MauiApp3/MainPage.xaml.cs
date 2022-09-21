0f2f96723ff7204ac3b0f84de86673a16cc82510
Jens-Christian Skibakk <jens.christian.skibakk@lexit.no>
2022-09-21 12:44:32 +0200

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new NavigationPage(new NewPage1()));
+    }
+}
+
